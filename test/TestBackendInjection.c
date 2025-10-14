f38c8a09ba7c7e99e545bad517e08669eea452dc
Vini Da-Silva <viniciusgoncalves_29@hotmail.com>
2025-10-13 17:22:27 -0700

    Simplify backend injection test by using putchar instead of custom backend

--- test/TestBackendInjection.c
+++ test/TestBackendInjection.c
@@ -1,47 +1,16 @@
 #include "unity.h"
 #include "log_c.h"
 #include <string.h>
-#include <stdarg.h>
+#include <stdio.h>
 
 static char capture_buffer[512];
 static size_t capture_pos;
 
-static int mock_vprintf(const char *fmt, va_list ap) {
-    /* Write formatted data into the capture buffer starting at capture_pos */
-    int written = vsnprintf(capture_buffer + capture_pos,
-                            sizeof(capture_buffer) - capture_pos,
-                            fmt, ap);
-    if (written > 0) {
-        size_t avail = sizeof(capture_buffer) - capture_pos;
-        if ((size_t)written >= avail) {
-            /* truncated */
-            capture_pos = sizeof(capture_buffer) - 1;
-            capture_buffer[capture_pos] = '\0';
-        } else {
-            capture_pos += (size_t)written;
-        }
+static void mock_putchar(char ch) {
+    if (capture_pos + 1 < sizeof(capture_buffer)) {
+        capture_buffer[capture_pos++] = ch;
+        capture_buffer[capture_pos] = '\0';
     }
-    return written;
-}
-
-static size_t mock_write(const char *buf, size_t len) {
-    size_t to_copy = len;
-    if (capture_pos + to_copy >= sizeof(capture_buffer)) {
-        to_copy = sizeof(capture_buffer) - 1 - capture_pos;
-    }
-    memcpy(capture_buffer + capture_pos, buf, to_copy);
-    capture_pos += to_copy;
-    capture_buffer[capture_pos] = '\0';
-    return to_copy;
-}
-
-static int mock_putchar(int ch) {
-    if (capture_pos + 1 >= sizeof(capture_buffer)) {
-        return EOF;
-    }
-    capture_buffer[capture_pos++] = (char)ch;
-    capture_buffer[capture_pos] = '\0';
-    return ch;
 }
 
 void setUp(void) {
@@ -51,17 +20,11 @@ void setUp(void) {
 
 void tearDown(void) {
     /* Restore default backend to avoid interference with other tests */
-    logc_set_backend(NULL);
+    logc_set_putchar(NULL);
 }
 
 void test_BackendInjection_CapturesLog(void) {
-    struct logc_backend mock = {
-        .vprintf = mock_vprintf,
-        .write = mock_write,
-        .putchar = mock_putchar,
-    };
-
-    logc_set_backend(&mock);
+    logc_set_putchar(mock_putchar);
 
     /* Log a known message */
     loginfo("Hello %s %d", "world", 123);
